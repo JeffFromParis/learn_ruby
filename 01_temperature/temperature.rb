0da97d53bc1f229bb91cb000f768ff711a6306bd
Jeff Vergne <vergnejf@gmail.com>
2019-05-28 15:24:45 +0200

    Completing exrcise 01 & 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc ftemp
+    ctemp = (5.0/9.0) * (ftemp-32)
+end
+
+def ctof ctemp
+    ftemp = (9.0/5.0) * ctemp + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+def add a,b
+    result = a + b
+end
+
+def subtract a,b
+    result = a - b
+end
+
+def sum array
+    result=0
+    array.each do |value|
+        result = result + value
+    end
+    result
+end
+
+def multiply array
+    result = 1
+    array.each do |value|
+        result = result * value
+    end
+    result
+end
+
+def power a,b
+    result = a**b
+end
+
+def factorial number
+    result=1
+    if number > 1
+        while number >=2 do
+            result=result*number
+            number=number-1
+        end
+    end
+    result
+end
