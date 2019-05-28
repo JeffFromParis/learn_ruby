986edb1ad0e18707ee9a91bc694ecd2b8535b834
Jeff Vergne <vergnejf@gmail.com>
2019-05-28 16:40:12 +0200

    completing exercise 06

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo word
+    word
+end
+
+def shout word
+    word.upcase
+end
+
+def repeat word, number=2
+    result = ([word] * number).join(" ")
+end
+
+def start_of_word word,index
+    result = word[0...index]
+end
+
+def first_word sentance
+    sentance.split(' ').first
+end
+
+def titleize sentance
+    words = sentance.split.map do |word|
+        if ['the', 'over', 'and'].include?(word)
+            word
+        else
+        word.capitalize
+        end
+    end
+    words.first.capitalize! #exclamation mark is used to modify the object it is called on 
+    words.join(" ")
+end
